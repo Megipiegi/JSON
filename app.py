83673aebc19f48292d5bed45769d5f6e93d2ef58
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-08-05 18:50:25 +0200

    poprawione wg komentarzy Piotra

--- app.py
+++ app.py
@@ -8,19 +8,10 @@ response = requests.get('https://api.nbp.pl/api/exchangerates/tables/C?format=js
 content = response.content
 data = json.loads(content)
 
+
+dict_rates = {}
 for x in (data[0]['rates']):
-    if (x['code'] == 'USD'):
-        print(x['bid'])
-        usdValue = x['bid']
-    elif (x['code'] == 'AUD'):
-        print(x['bid'])
-        audValue = x['bid']
-    elif (x['code'] == 'EUR'):
-        print(x['bid'])
-        eurValue = x['bid']
-    elif (x['code'] == 'CAD'):
-        print(x['bid'])
-        cadValue = x['bid']
+    dict_rates[x['code']]=x['bid']
 
 app = Flask(__name__)
 
@@ -36,20 +27,13 @@ def message ():
 
 @app.route ('/result', methods = ['POST'])
 def form ():
-    currencyCode=request.form.get('currencyCode')
-    currencyAmount=request.form.get('currencyAmount')
-    if (currencyCode == 'USD'):
-        currencyValue = usdValue
-    elif (currencyCode == 'AUD'):
-        currencyValue = audValue
-    elif (currencyCode == 'EUR'):
-        currencyValue = eurValue
-    elif (currencyCode == 'CAD'):
-        currencyValue = cadValue
-    else:
-        currencyValue = 0
-    totalValuePln = float(currencyValue) * float(currencyAmount)
-    print(totalValuePln)
+    currency_code=request.form.get('currency_code')
+    currency_amount=request.form.get('currency_amount')
+
+    total_value_pln = float(dict_rates[currency_code]) * float(currency_amount)
+    total_value_pln = round(total_value_pln, 2)
+    print(total_value_pln)
+    
 
     title = "Kalkulator"
-    return render_template('result.html', title=title, currencyCode=currencyCode, currencyAmount=currencyAmount, totalValuePln=totalValuePln)
+    return render_template('result.html', title=title, currency_code=currency_code, currency_amount=currency_amount, total_value_pln=total_value_pln)
